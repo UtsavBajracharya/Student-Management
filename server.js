caebc7a280b02be64fbcebe8e2a1bcab2d3803b5
Utsav <utsavbajra619@gmail.com>
2024-11-18 23:50:50 -0500

    add mongoose connection in sever

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
@@ -9,6 +10,15 @@ const PORT = 3000;
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
+
+// Connect to MongoDB
+mongoose.connect("mongodb://127.0.0.1:27017/studentDB", { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.error(err));
+
+// Routes
+app.use("/api/students", require("./routes/students"));
+app.use("/api/courses", require("./routes/courses"));
 
 // Start Server
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
